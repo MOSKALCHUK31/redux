ff506e69cd82768509e852d709216ba2806b5628
Sergey Moskalchuk <66484544+MOSKALCHUK31@users.noreply.github.com>
2024-09-20 19:46:22 +0300

    init

--- src/App.js
+++ src/App.js
@@ -1,25 +1,69 @@
-import logo from './logo.svg';
-import './App.css';
+import './App.css'
+import {useDispatch, useSelector} from 'react-redux';
+import {addCustomerAction, removeCustomerAction} from './store/customerReducer'
+import {fetchCustomers} from './asyncActions/customers'
 
 function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+    const dispatch = useDispatch()
+    const cash = useSelector((state) => state.cash.cash)
+    const customers = useSelector((state) => state.customers.customers)
+
+    const addCash = () => {
+        dispatch({ type: 'ADD_CASH', payload: Number(prompt()) })
+    }
+
+    const getCash = () => {
+        dispatch({ type: 'GET_CASH', payload: Number(prompt()) })
+    }
+
+    const addCustomer = () => {
+        dispatch(addCustomerAction({
+            name: prompt(),
+            id: new Date()
+        }))
+    }
+
+    const removeCustomer = (id) => {
+        dispatch(removeCustomerAction(id))
+    }
+
+    const getCustomers = async () => {
+        dispatch(fetchCustomers())
+    }
+
+    return (
+        <div className="App">
+            <div>
+                {cash}
+            </div>
+            <div>
+                <button onClick={addCash}>
+                    ADD BALANCE
+                </button>
+                <button onClick={getCash}>
+                    GET BALANCE
+                </button>
+            </div>
+            <br/><br/>
+            <div>
+                <button onClick={addCustomer}>
+                    ADD CUSTOMER
+                </button>
+                <button onClick={getCustomers}>
+                    GET MANY CUSTOMERS => ...
+                </button>
+            </div>
+            <div>
+                <ul>
+                    {customers.map((c) => (
+                        <li onClick={() => removeCustomer(c.id)} key={c.id}>
+                            {c.name}
+                        </li>
+                    ))}
+                </ul>
+            </div>
+        </div>
+    )
 }
 
-export default App;
+export default App
